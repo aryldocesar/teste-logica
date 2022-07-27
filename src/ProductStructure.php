8f9b4b8069a4a9439bf4280f8f2ced93f1d3cb12
Aryldo César <arildocesar@gmail.com>
2022-07-27 17:54:43 -0300

    Update ProductStructure.php

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -26,15 +26,7 @@ class ProductStructure
         $array = array_count_values(static::products);
         foreach($array as $key => $item){
             $product_item = explode('-', $key);
-            if (isset($products_array[$product_item[0]])){
-                if(array_key_exists($product_item[1], $products_array[$product_item[0]])){
-                    $products_array[$product_item[0]][$product_item[1]] = $item;
-                }else{
-                    $products_array[$product_item[0]][$product_item[1]] = $item;
-                }
-            }else{
-                $products_array[$product_item[0]][$product_item[1]] =$item;
-            }
+            $products_array[$product_item[0]][$product_item[1]] =$item;  
         }
         return $products_array;
     }
